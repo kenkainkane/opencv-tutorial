d6c884b26647d37b4600d39a2294cc2204a81add
Wattanai Sathuphan <wattanai.sat@ku.th>
2018-10-30 00:41:05 +0700

    open both of image & del unnecessary space
    print() in python3 already add a space

--- tutorial/3_storage.py
+++ tutorial/3_storage.py
@@ -7,13 +7,14 @@ gray = cv2.imread('../images/apple.jpg',0)
 # image.shape ใช้ตรวจสอบ row, column, channel ของภาพที่เก็บไว้ในตัวแปร
 # จะเห็นได้ว่า ภาพสีจะถูกระบุไว้ทั้ง row, column และ channel
 # ส่วนภาพ Grayscale จะถูกระบุแค่ row และ column
-print('img ', img.shape)
-print('gray ', gray.shape)
+print('img', img.shape)
+print('gray', gray.shape)
 
 # เราสามารถทราบได้ว่าที่ row, column ที่ต้องการตรวจสอบมีค่าสีเท่าไหร่
-print('img color ', img[220, 220])
-print('gray color ', gray[220, 220])
+print('img color', img[220, 220])
+print('gray color', gray[220, 220])
 
 cv2.imshow('img', img)
+cv2.imshow('gray', gray)
 cv2.waitKey()
 cv2.destroyAllWindows()
